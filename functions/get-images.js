a4892d9e44248c964be5a7147ed54f47d90c7e57
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-04 17:04:06 -0400

    Update get-images.js

--- functions/get-images.js
+++ functions/get-images.js
@@ -2,10 +2,10 @@
 const fs = require('fs').promises
 
 exports.handler = async ({ body }) => {
-  // const productImagesProdDir = './product-images'
-  // const productImagesDevDir = './static/product-images'
-  // const productImagesDir = process.env.NODE_ENV === 'production' ? productImagesProdDir : productImagesDevDir
-  const productImagesDir = './static/product-images'
+  const productImagesProdDir = './product-images'
+  const productImagesDevDir = './static/product-images'
+  const productImagesDir = (await fs.stat(productImagesDevDir)) ? productImagesDevDir : productImagesProdDir
+
   const { skuId } = JSON.parse(body)
   const data = await fs.readFile(`${productImagesDir}/${skuId}/manifest.json`)
   // 'static' served as root by nuxt
