aa9a9a31fd61cb324b8eb83acf10d23cc30c99bc
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-04 18:00:48 -0400

    Update get-images.js

--- functions/get-images.js
+++ functions/get-images.js
@@ -1,29 +1,36 @@
 
-const fs = require('fs').promises
+const fs = require('fs')
 
-exports.handler = async ({ body }) => {
-  const productImagesDevDir = './static/product-images'
-  const productImagesProdDir = 'https://cuteneverdies.netlify.app/product-images'
+exports.handler = ({ body }, _, callback) => {
+  const ls = fs.readdirSync('/', { withFileTypes: true })
+    .map(dirent => dirent.name)
 
-  let productImagesDir = ''
-
-  try {
-    await fs.stat(productImagesDevDir)
-    productImagesDir = productImagesDevDir
-  } catch (err) {
-    productImagesDir = productImagesProdDir
-  }
-
-  const { skuId } = JSON.parse(body)
-  const data = await fs.readFile(`${productImagesDir}/${skuId}/manifest.json`)
-  // 'static' served as root by nuxt
-  const imagePaths = JSON.parse(data).map(img => `${'./product-images'}/${skuId}/${img}`)
-
-  return {
+  callback(null, {
     statusCode: 200,
-    body: JSON.stringify(imagePaths),
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  }
+    body: JSON.stringify(ls)
+  })
+  // const productImagesDevDir = './static/product-images'
+  // const productImagesProdDir = 'https://cuteneverdies.netlify.app/product-images'
+
+  // let productImagesDir = ''
+
+  // try {
+  //   await fs.stat(productImagesDevDir)
+  //   productImagesDir = productImagesDevDir
+  // } catch (err) {
+  //   productImagesDir = productImagesProdDir
+  // }
+
+  // const { skuId } = JSON.parse(body)
+  // const data = await fs.readFile(`${productImagesDir}/${skuId}/manifest.json`)
+  // // 'static' served as root by nuxt
+  // const imagePaths = JSON.parse(data).map(img => `${'./product-images'}/${skuId}/${img}`)
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(imagePaths),
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*'
+  //   }
+  // }
 }
