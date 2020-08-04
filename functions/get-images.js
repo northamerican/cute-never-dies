ab859690185cba0a1ed4e766ce8d5ac143884f5f
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-04 16:21:13 -0400

    Update get-images.js

--- functions/get-images.js
+++ functions/get-images.js
@@ -2,14 +2,14 @@
 const fs = require('fs').promises
 
 exports.handler = async ({ body }) => {
-  const productImagesProdDir = './product-images'
-  const productImagesDevDir = './static/product-images'
+  // const productImagesProdDir = './product-images'
+  // const productImagesDevDir = './static/product-images'
   // const productImagesDir = process.env.NODE_ENV === 'production' ? productImagesProdDir : productImagesDevDir
-  const productImagesDir = productImagesProdDir
+  const productImagesDir = './static/product-images'
   const { skuId } = JSON.parse(body)
   const data = await fs.readFile(`${productImagesDir}/${skuId}/manifest.json`)
   // 'static' served as root by nuxt
-  const imagePaths = JSON.parse(data).map(img => `${productImagesProdDir}/${skuId}/${img}`)
+  const imagePaths = JSON.parse(data).map(img => `${'./product-images'}/${skuId}/${img}`)
 
   return {
     statusCode: 200,
