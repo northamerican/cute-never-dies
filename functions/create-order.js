2105b3d12503b4133a6401423ff631f420e4e78c
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-12 19:18:20 -0400

    stripe maxNetworkRetries: 2

--- functions/create-order.js
+++ functions/create-order.js
@@ -3,7 +3,8 @@ const secretKey = process.env.STRIPE_IS_LIVE_MODE === 'true'
   : process.env.STRIPE_SECRET_KEY_TEST
 
 const stripe = require('stripe')(secretKey, {
-  apiVersion: process.env.STRIPE_API_VERSION
+  apiVersion: process.env.STRIPE_API_VERSION,
+  maxNetworkRetries: 2
 })
 
 const headers = {
