f800ac2b082483cb284169a077da53acf192149c
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-13 03:00:51 -0400

    Update get-shipping-rates.js

--- functions/get-shipping-rates.js
+++ functions/get-shipping-rates.js
@@ -1,8 +1,8 @@
 // Configured in https://dashboard.stripe.com/settings/orders
 // ! Deprecated - https://stripe.com/docs/orders/dynamic-shipping-taxes
 
-// const baseCurrency = 'CAD'
-// const freeShippingCountries = ['CA', 'US']
+const baseCurrency = 'CAD'
+const freeShippingCountries = ['CA', 'US']
 // const freeShippingCountries = process.env.FREE_SHIPPING_COUNTRIES.split(',')
 
 const headers = {
@@ -10,45 +10,45 @@ const headers = {
 }
 
 exports.handler = async ({ body }) => {
-  // const jsonBody = JSON.parse(body)
-  // const { country } = jsonBody.order.shipping.address
-  // const isFreeShipping = freeShippingCountries.includes(country)
+  const jsonBody = JSON.parse(body)
+  const { country } = jsonBody.order.shipping.address
+  const isFreeShipping = freeShippingCountries.includes(country)
 
-  // const tax = {
-  //   parent: null,
-  //   type: 'tax',
-  //   description: 'Taxes (included)',
-  //   amount: 0,
-  //   currency: baseCurrency.toLowerCase()
-  // }
-  // const shippingMethod = isFreeShipping ? {
-  //   id: 'free_shipping',
-  //   description: 'Free shipping',
-  //   amount: 0,
-  //   currency: baseCurrency.toLowerCase(),
-  //   tax_items: []
-  // } : {
-  //   id: 'intl_shipping',
-  //   description: 'International shipping',
-  //   amount: 3800,
-  //   currency: baseCurrency.toLowerCase(),
-  //   tax_items: []
-  // }
+  const tax = {
+    parent: null,
+    type: 'tax',
+    description: 'Taxes (included)',
+    amount: 0,
+    currency: baseCurrency.toLowerCase()
+  }
+  const shippingMethod = isFreeShipping ? {
+    id: 'free_shipping',
+    description: 'Free shipping',
+    amount: 0,
+    currency: baseCurrency.toLowerCase(),
+    tax_items: []
+  } : {
+    id: 'intl_shipping',
+    description: 'International shipping',
+    amount: 3800,
+    currency: baseCurrency.toLowerCase(),
+    tax_items: []
+  }
 
-  // const response = {
-  //   order_update: {
-  //     items: [
-  //       tax
-  //     ],
-  //     shipping_methods: [
-  //       shippingMethod
-  //     ]
-  //   }
-  // }
+  const response = {
+    order_update: {
+      items: [
+        tax
+      ],
+      shipping_methods: [
+        shippingMethod
+      ]
+    }
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ a: 2 }),
+    body: JSON.stringify(response),
     headers
   }
 }
