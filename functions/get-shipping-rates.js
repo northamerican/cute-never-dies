d7c1590f6fd22b0da54bc594aff7a79bb72fe695
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-13 02:26:07 -0400

    test shipping rate response

--- functions/get-shipping-rates.js
+++ functions/get-shipping-rates.js
@@ -6,44 +6,44 @@ const freeShippingCountries = ['CA', 'US']
 // const freeShippingCountries = process.env.FREE_SHIPPING_COUNTRIES.split(',')
 
 exports.handler = ({ body }) => {
-  const jsonBody = JSON.parse(body)
-  const { country } = jsonBody.order.shipping.address
-  const isFreeShipping = freeShippingCountries.includes(country)
+  // const jsonBody = JSON.parse(body)
+  // const { country } = jsonBody.order.shipping.address
+  // const isFreeShipping = freeShippingCountries.includes(country)
 
-  const tax = {
-    parent: null,
-    type: 'tax',
-    description: 'Taxes (included)',
-    amount: 0,
-    currency: baseCurrency.toLowerCase()
-  }
-  const shippingMethod = isFreeShipping ? {
-    id: 'free_shipping',
-    description: 'Free shipping',
-    amount: 0,
-    currency: baseCurrency.toLowerCase(),
-    tax_items: []
-  } : {
-    id: 'intl_shipping',
-    description: 'International shipping',
-    amount: 3800,
-    currency: baseCurrency.toLowerCase(),
-    tax_items: []
-  }
+  // const tax = {
+  //   parent: null,
+  //   type: 'tax',
+  //   description: 'Taxes (included)',
+  //   amount: 0,
+  //   currency: baseCurrency.toLowerCase()
+  // }
+  // const shippingMethod = isFreeShipping ? {
+  //   id: 'free_shipping',
+  //   description: 'Free shipping',
+  //   amount: 0,
+  //   currency: baseCurrency.toLowerCase(),
+  //   tax_items: []
+  // } : {
+  //   id: 'intl_shipping',
+  //   description: 'International shipping',
+  //   amount: 3800,
+  //   currency: baseCurrency.toLowerCase(),
+  //   tax_items: []
+  // }
 
-  const response = {
-    order_update: {
-      items: [
-        tax
-      ],
-      shipping_methods: [
-        shippingMethod
-      ]
-    }
-  }
+  // const response = {
+  //   order_update: {
+  //     items: [
+  //       tax
+  //     ],
+  //     shipping_methods: [
+  //       shippingMethod
+  //     ]
+  //   }
+  // }
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
+    body: '{"a": 1}'
   }
 }
