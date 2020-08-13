783dd369fde65e4f2b6ff3f55c6d48396b77869e
Chris Bitsakis <c.bitsakis@gmail.com>
2020-08-12 22:05:43 -0400

    fix sku page view bug

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,45 +1,49 @@
 <template>
   <main>
-    <section v-if="skuId" class="section">
-      <div class="container">
-        <sku-page :sku="getSkuById(skuId)" />
-      </div>
-    </section>
-
-    <section v-else class="hero is-primary">
-      <div class="hero-body">
+    <template v-if="skuId">
+      <section class="section">
         <div class="container">
-          <h1 class="title">
-            {{ $t('Shop') }} {{ skuId }}
+          <sku-page :sku="getSkuById(skuId)" />
+        </div>
+      </section>
+    </template>
+
+    <template v-else>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              {{ $t('Shop') }} {{ skuId }}
+            </h1>
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <div class="product-row container">
+          <!-- <h1 class="title">
+            {{ product.name }}
           </h1>
+          <h2 class="subtitle">
+            {{ product.caption }}
+          </h2> -->
+          <div class="columns is-multiline">
+            <article v-for="sku in skus" :key="sku.id" class="column is-one-third-desktop is-half-tablet">
+              <sku-tile :sku="sku" />
+            </article>
+          </div>
         </div>
-      </div>
-    </section>
-    <section class="section">
-      <div class="product-row container">
-        <!-- <h1 class="title">
-          {{ product.name }}
-        </h1>
-        <h2 class="subtitle">
-          {{ product.caption }}
-        </h2> -->
-        <div class="columns is-multiline">
-          <article v-for="sku in skus" :key="sku.id" class="column is-one-third-desktop is-half-tablet">
-            <sku-tile :sku="sku" />
-          </article>
+      </section>
+      <!-- <section class="section">
+        <div class="container has-text-centered">
+          <div class="content">
+            <h1 class="title">
+              More creatures coming soon...
+            </h1>
+            <h2>😻</h2>
+          </div>
         </div>
-      </div>
-    </section>
-    <!-- <section class="section">
-      <div class="container has-text-centered">
-        <div class="content">
-          <h1 class="title">
-            More creatures coming soon...
-          </h1>
-          <h2>😻</h2>
-        </div>
-      </div>
-    </section> -->
+      </section> -->
+    </template>
   </main>
 </template>
 
